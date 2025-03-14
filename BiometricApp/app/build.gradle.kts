4d3b42f17804065174e6de8bc3ee1c29bec5ff1d
Karthiga Ganesan <57249842+karthigaganesh1997@users.noreply.github.com>
2025-03-14 22:34:02 +0530

    Add files via upload

--- BiometricApp/app/build.gradle.kts
+++ BiometricApp/app/build.gradle.kts
@@ -1,84 +1,8 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.dagger.android.app)
-
-    id("kotlin-kapt")
-
-}
-
-android {
-    namespace = "com.example.biometricapp"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "com.example.biometricapp"
-        minSdk = 29
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        compose = true
-    }
-}
-
-dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    implementation(libs.androidx.appcompat)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
-
-    //  Navigation
-    implementation(libs.androidx.navigation.compose)
-
-    //  Preferences
-    implementation(libs.datastore.preferences)
-
-    //biometric
-
-    implementation(libs.androidx.biometric)
-
-
-    //dagger hilt
-    implementation(libs.dagger.hilt)
-    implementation(libs.dagger.hilt.navigation)
-    kapt(libs.dagger.compiler)
-
-    //json
-    implementation(libs.google.json)
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.dagger.android.app) apply false
 
 }
